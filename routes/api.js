bd71c03ffbdf8dbfb37a1394dfea46ae6db9405a
march-dave <ysangy@gmail.com>
2016-05-21 23:55:59 -0700

    add AWS etc

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/emotion', require('./emotion'));
+// router.use('/S3', require('./S3'));
 
 module.exports = router;
-  
